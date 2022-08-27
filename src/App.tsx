1d159d725831620b41332c0984d0b0fae17a2107
Mike Green <greens1593@gmail.com>
2022-08-27 14:45:24 +0300

    Created modal window to add product

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { CrateProduct } from './component/CreateProduct';
 import { ErrorMessage } from './component/ErrorMessage';
 import { Loader } from './component/Loader';
+import { Modal } from './component/Modal';
 import {Product} from './component/Product'
 import {useProducts} from './hooks/product'
+import { IProduct } from './models';
 
 function App() {
-  const {loading, error, products} = useProducts()
-
+  const {loading, error, products, addProduct} = useProducts()
+  const [modal, setModal] = useState(false)
+  const createHandler = (product: IProduct) => {
+    setModal(false)
+    addProduct(product)
+  }
   return (
     <div className='containet mx-auto max-w-2xl pt-5'> 
     {loading && <Loader/>}
     {error && <ErrorMessage error={error}/>}
     {products.map(product => <Product product={product} key={product.id}/>)}
-
+    {modal && <Modal title="Create new product" onClose={()=>setModal(false)}>
+      <CrateProduct onCreate={createHandler}/>
+    </Modal>}
+    <button 
+    className='fixed bottom-5 right-5 rounded-full bg-red-700 text-white text-2xl px-4 py-2'
+    onClick={()=>setModal(true)}
+    >+</button>
     </div>
   )
 }
--- src/hooks/product.ts
+++ src/hooks/product.ts
@@ -5,8 +5,12 @@ import { IProduct } from '../models';
 
 export function useProducts () {
     const [products, setProducts] = useState<IProduct[]>([])
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState('')
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState('')
+
+    function addProduct(product: IProduct){
+        setProducts(prev => [...prev, product])
+    }
 
   async function fetchProducts() {
     try {
@@ -24,5 +28,5 @@ export function useProducts () {
   useEffect(() => {
     fetchProducts()
   }, [])
-  return {products, error, loading}
+  return {products, error, loading, addProduct}
 }
