57082c6a91586009a561ebf1ccc74405d5585794
Mike Green <greens1593@gmail.com>
2022-08-27 15:59:14 +0300

    Context was added in the app. Router and navigation was added

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function AboutPage() {
+    return (
+        <div>
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus rem, impedit eius vitae iure culpa maiores doloremque officia delectus ab modi autem aperiam ut optio nemo ducimus soluta eum. A.</h1>
+        </div>
+    )
+}
---
+++ src/pages/ProductPage.tsx
@@ -0,0 +1,34 @@
+import React, { useContext } from 'react'
+import { CrateProduct } from '../component/CreateProduct';
+import { ErrorMessage } from '../component/ErrorMessage';
+import { Loader } from '../component/Loader';
+import { Modal } from '../component/Modal';
+import {Product} from '../component/Product'
+import { ModalContext } from '../context/ModelContext';
+import {useProducts} from '../hooks/product'
+import { IProduct } from '../models';
+
+export function ProductPage () {
+    const {loading, error, products, addProduct} = useProducts()
+  
+    const {modal, open, close} = useContext(ModalContext)
+
+    const createHandler = (product: IProduct) => {
+        close()
+        addProduct(product)
+    }
+  return (
+    <div className='containet mx-auto max-w-2xl pt-5'> 
+    {loading && <Loader/>}
+    {error && <ErrorMessage error={error}/>}
+    {products.map(product => <Product product={product} key={product.id}/>)}
+    {modal && <Modal title="Create new product" onClose={()=>close()}>
+      <CrateProduct onCreate={createHandler}/>
+    </Modal>}
+    <button 
+    className='fixed bottom-5 right-5 rounded-full bg-red-700 text-white text-2xl px-4 py-2'
+    onClick={()=>open()}
+    >+</button>
+    </div>
+  )
+}
